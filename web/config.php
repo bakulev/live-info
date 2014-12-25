37b60b2b7d77974f7007014e68922a34f3ee9641
Anton Bakulev <bakulev@mipt.ru>
2014-12-25 10:49:34 +0300

    Указан IP-адрес для доступа к окружению разработчика

--- web/config.php
+++ web/config.php
@@ -7,8 +7,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-	'109.188.125.21',
-))|| !(in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('109.188.125.21'))) {
+))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
 }
