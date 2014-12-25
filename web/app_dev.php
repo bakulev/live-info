42f21af72de9fbb3bfab5c89f946958fce720ee6
Anton Bakulev <bakulev@mipt.ru>
2014-12-25 10:29:25 +0300

    Указан IP-адрес для доступа к окружению разработчика

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,8 +12,9 @@ use Symfony\Component\Debug\Debug;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     //|| isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '109.188.125.21', 'fe80::1', '::1'))
-    || !(in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('109.188.125.21'))
-    || php_sapi_name() === 'cli-server')
+        || in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], array('109.188.125.21'))
+        || php_sapi_name() === 'cli-server'
+	)
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
